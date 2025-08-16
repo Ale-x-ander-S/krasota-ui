3d8395ec05b688a553f7cb95ffe7756ad8bd126a
AlexanderS <red52@bk.ru>
2025-08-17 00:13:50 +0500

    Добавлены индексы компонентов

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,14 +2,14 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/products', pathMatch: 'full' },
-  { path: 'products', loadComponent: () => import('./pages/products/products.component').then(m => m.ProductsComponent) },
-  { path: 'product/:id', loadComponent: () => import('./pages/product-detail/product-detail.component').then(m => m.ProductDetailComponent) },
-  { path: 'categories', loadComponent: () => import('./pages/categories/categories.component').then(m => m.CategoriesComponent) },
-  { path: 'about', loadComponent: () => import('./pages/about/about.component').then(m => m.AboutComponent) },
-  { path: 'contacts', loadComponent: () => import('./pages/contacts/contacts.component').then(m => m.ContactsComponent) },
-  { path: 'cart', loadComponent: () => import('./pages/cart/cart.component').then(m => m.CartComponent) },
-  { path: 'checkout', loadComponent: () => import('./pages/checkout/checkout.component').then(m => m.CheckoutComponent) },
-  { path: 'auth', loadComponent: () => import('./pages/auth/auth.component').then(m => m.AuthComponent) },
-  { path: 'profile', loadComponent: () => import('./pages/profile/profile.component').then(m => m.ProfileComponent) },
+  { path: 'products', loadComponent: () => import('./pages/products').then(m => m.ProductsComponent) },
+  { path: 'product/:id', loadComponent: () => import('./pages/product-detail').then(m => m.ProductDetailComponent) },
+  { path: 'categories', loadComponent: () => import('./pages/categories').then(m => m.CategoriesComponent) },
+  { path: 'about', loadComponent: () => import('./pages/about').then(m => m.AboutComponent) },
+  { path: 'contacts', loadComponent: () => import('./pages/contacts').then(m => m.ContactsComponent) },
+  { path: 'cart', loadComponent: () => import('./pages/cart').then(m => m.CartComponent) },
+  { path: 'checkout', loadComponent: () => import('./pages/checkout').then(m => m.CheckoutComponent) },
+  { path: 'auth', loadComponent: () => import('./pages/auth').then(m => m.AuthComponent) },
+  { path: 'profile', loadComponent: () => import('./pages/profile').then(m => m.ProfileComponent) },
   { path: '**', redirectTo: '/products' }
 ];
